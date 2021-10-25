230c96c8a7eaec18bd432d00f45659fba935eff8
Vivek Shah <vivekshahcodes@gmail.com>
2021-10-25 11:48:45 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+$("a.navbar-brand").hover(function(){
+    $(this).css("color", "#F50057");
+    }, function(){
+    $(this).css("color", "white");
+  });
+
+$("h1").hover(function(){
+    $(this).css("color", "#F50057");
+    }, function(){
+    $(this).css("color", "white");
+  });
+
+$("h2").hover(function(){
+    $(this).css("color", "#F50057");
+    }, function(){
+    $(this).css("color", "black");
+  });
+
+$("section.bg-dark h2").hover(function(){
+    $(this).css("color", "#F50057");
+    }, function(){
+    $(this).css("color", "white");
+  });
